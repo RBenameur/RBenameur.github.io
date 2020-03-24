d8c50901b77e8e2adcfe29f062f4d3145acee207
RBenameur <benamer01@gmail.com>
2020-03-24 11:49:43 +0000

    update

--- getInvolvedPage/charity.js
+++ getInvolvedPage/charity.js
@@ -1,25 +1,26 @@
+function initMap () {
+  var options = {
+    zoom:10,
+    center:{lat:51.5074,lng:-0.118092}
+  }
+  var map = new google.maps.Map(document.getElementById('map'), options);
+}
 $( document ).ready(function() {
   console.log("hi");
-  /*$('#submitGetInvolved').click( function() {
-    console.log('submit');
-});*/
-$('#submitEnquiryForm').onclick( function( event ) {
-  event.stopPropagation();
-  buttonFuction(this.id);
-   console.log('submit');
-  });
+
 $( '#getInvolvedForm' ).submit(function( event ) {
   event.preventDefault();
   var getInvolvedVar = $(this).serializeArray();
   console.log(getInvolvedVar);
   console.log('hi');
   alert('thank you '+ getInvolvedVar[0].value);
-});
+}); 
 $( '#enquiryForm' ).submit(function( event ) {
   event.preventDefault();
   var enquiryFormVar = $(this).serializeArray();
   console.log(enquiryFormVar);
   console.log('hi');
   alert('thank you '+ enquiryFormVar[0].value);
+  console.log('submit');
 });
 });
